a241d250237e4cedaa26539927acdee5b33d21e5
Shah <krati.shah@capgemini.com>
2018-05-17 17:36:18 +0530

    added setup

---
+++ scripts/setup.py
@@ -0,0 +1,12 @@
+# This file is responsible for installing all necessary modules needed for running create_release.py script.
+import os
+import sys
+
+os.chdir(os.path.dirname(sys.executable))
+os.chdir("Scripts")
+os.system('pip install gitpython')
+os.system('pip install PyGithub')
+os.system('pip install lxml')
+os.system('pip install pathlib')
+os.system('pip install uritemplate')
+os.system('pip install requests')
